2e66b9f0046682441f09c8d0f08468116491b57f
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2024-10-07 20:22:03 +0530

    Add files via upload

---
+++ Nimap_ FInal_Project/src/main/java/com/example/nimapfinalproject/NimapFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nimapfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimapFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NimapFinalProjectApplication.class, args);
+    }
+
+}
