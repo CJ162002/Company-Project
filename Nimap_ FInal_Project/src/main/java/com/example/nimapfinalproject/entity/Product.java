e1dd1f8f6b46de415dcddf170506145ff2dd77aa
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2024-10-09 20:49:42 +0530

    Add files via upload

--- Nimap_ FInal_Project/src/main/java/com/example/nimapfinalproject/entity/Product.java
+++ Nimap_ FInal_Project/src/main/java/com/example/nimapfinalproject/entity/Product.java
@@ -11,7 +11,6 @@ public class Product implements Serializable {
     private Long id;
     private String name;
     private Double price;
-    @JsonIgnore
     @ManyToOne()
     @JoinColumn(name = "category_id")
     private Category category;
